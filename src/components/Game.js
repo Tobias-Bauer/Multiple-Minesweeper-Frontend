27bdc262dd8f2b016e280b20b27eecc00902fcdf
Tobias-Bauer <40526821+Tobias-Bauer@users.noreply.github.com>
2021-01-06 13:04:06 +0100

    first commit

---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+			</div>
+        )
+    }
+}
---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Home.scss'
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { id: null, messages: [], n_cols: "", n_rows: "", code: "", n_mines: "", solvable: false }
+    }
+    componentDidMount() {
+        this.connection = new WebSocket('ws://127.0.0.1:8000/create');
+        // listen to onmessage event
+        this.connection.onmessage = evt => {
+            console.log(evt)
+            let data = JSON.parse(evt.data)
+            console.log(data)
+            if (data.error) {
+                alert(evt.error)
+            }
+            if (data.succes === "Game succesfully created") {
+                window.open('/game/'+this.state.code, '_self', 'noopener,noreferrer')
+            }
+        };
+        // window.open('/game/'+this.state.id, '_self', 'noopener,noreferrer')
+    }
+    render() {
+        return (
+            <div>
+                <h1>Welcome to our multiple player minesweeper games!</h1>
+                <p>Create a new game</p>
+                <input className="size" value={this.state.n_cols} onChange={(e) => this.setState({ n_cols: e.target.value })} type="number" placeholder="Size between 3 and 60" />
+                <input className="size" value={this.state.n_rows} onChange={(e) => this.setState({ n_rows: e.target.value })} type="number" placeholder="Size between 3 and 60" />
+                <input className="size" value={this.state.n_mines} onChange={(e) => this.setState({ n_mines: e.target.value })} type="number" placeholder="Mines between 1 and 2600" />
+                <input type="checkbox" id="check3" value={this.state.solvable} onChange={(e) => this.setState({ solvable: e.target.value })} name="fsa" />
+                <label htmlFor="check3">solvable</label>
+                <button onClick={() => this.connection.send(JSON.stringify({ n_cols: this.state.n_cols, n_rows: this.state.n_rows, n_mines: this.state.n_mines, code: this.state.code, solvable: this.state.solvable }))}>Create</button>
+                <p>Join an existing game</p>
+                <input value={this.state.code} onChange={(e) => this.setState({ code: e.target.value })} type="number" placeholder="Code to join" />
+                <button onClick={() => this.connection.send({ code: this.state.code })}>Join</button>
+                {this.state.messages}
+            </div>
+        )
+    }
+}
