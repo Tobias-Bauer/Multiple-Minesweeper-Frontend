6ef56450af693742602668f279cb4c4598b1785a
Tobias-Bauer <40526821+Tobias-Bauer@users.noreply.github.com>
2021-01-11 15:40:20 +0100

    Show game status feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import './Home.scss'
 export default class Home extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { id: null, messages: [], n_cols: "", n_rows: "", code: "", n_mines: "", solvable: false }
+        this.state = { exists: null, id: null, messages: [], n_cols: "", n_rows: "", code: "", n_mines: "", solvable: false }
     }
-    async create_new_game() {
+    create_new_game() {
         fetch(this.props.domain + '/create', {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             headers: {
@@ -54,6 +54,8 @@ export default class Home extends React.Component {
                     }
                     if (result.exists) {
                         window.open('/game/' + this.state.code, '_self', 'noopener,noreferrer')
+                    } else {
+                        alert("Game doesn't exist!")
                     }
                 },
                 // Note: it's important to handle errors here
@@ -64,21 +66,60 @@ export default class Home extends React.Component {
                 }
             )
     }
+    componentWillMount() {
+        this.timer = null;
+    }
+    handleChange(e) {
+        clearTimeout(this.timer);
+        this.setState({ code: e.target.value })
+        this.timer = setTimeout(this.triggerAvailable(this.props.domain), 1000);
+    }
+    handleKeyDown(e) {
+        if (e.keyCode === 13) {
+            this.triggerChange();
+        }
+    }
+    triggerAvailable(domain) {
+        fetch(domain + '/join', {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*"
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({ code: this.state.code }) // body data type must match "Content-Type" header
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result)
+                    if (result.error) {
+                        alert(result.error)
+                    }
+                    this.setState({exists: result.exists})
+                },
+                (error) => {
+                    console.log(error)
+                }
+            )
+    }
     render() {
         return (
             <div>
-                <h1>Welcome to our multiple player minesweeper games!</h1>
+                <h1>Welcome to our multiplayer minesweeper game!</h1>
                 <p>Create a new game</p>
                 <input className="size" value={this.state.n_cols} onChange={(e) => this.setState({ n_cols: e.target.value })} type="number" placeholder="Size between 5 and 60" />
                 <input className="size" value={this.state.n_rows} onChange={(e) => this.setState({ n_rows: e.target.value })} type="number" placeholder="Size between 5 and 60" />
                 <input className="size" value={this.state.n_mines} onChange={(e) => this.setState({ n_mines: e.target.value })} type="number" placeholder="Mines between 1 and 2600" />
                 <input type="checkbox" id="check3" value={this.state.solvable} onChange={(e) => this.setState({ solvable: e.target.value })} name="fsa" />
                 <label htmlFor="check3">solvable</label>
-                <input value={this.state.code} onChange={(e) => this.setState({ code: e.target.value })} type="number" placeholder="Code to join" />
+                <input value={this.state.code} onChange={(e) => this.handleChange(e)} type="number" placeholder="Code to join" />
                 <button onClick={() => this.create_new_game()}>Create</button>
+                {this.state.exists?<p>{this.state.exists?"🔴":"🟢"}</p>:null}
                 <p>Join an existing game</p>
-                <input value={this.state.code} onChange={(e) => this.setState({ code: e.target.value })} type="number" placeholder="Code to join" />
+                <input value={this.state.code} onChange={(e) => this.handleChange(e)} type="number" placeholder="Code to join" />
                 <button onClick={() => this.join_game()}>Join</button>
+                {this.state.exists?<p>{this.state.exists?"🟢":"🔴"}</p>:null}
                 {this.state.messages}
             </div>
         )
